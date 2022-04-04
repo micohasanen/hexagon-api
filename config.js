5ecfec6f79d368c0db0504e8dc7efc6c5dd2e6a8
Mico Häsänen <micohasanen@gmail.com>
2022-04-05 00:21:23 +0300

    Added redis user field

--- config.js
+++ config.js
@@ -2,9 +2,11 @@ module.exports = {
   redisConnection: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USER,
     password: process.env.REDIS_PASS,
     connectTimeout: 10000,
-    lazyConnect: true
+    lazyConnect: true,
+    tls: {}
   },
   marketplaces: {
     '80001': process.env.MARKETPLACE_MUMBAI, 
