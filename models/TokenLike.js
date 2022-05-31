23b46809eac7f4f82fe187164b6b47ced3d5633f
Recursive <recursivethedev@gmail.com>
2022-05-31 11:06:11 +0300

    DONE !

---
+++ models/TokenLike.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+
+const TokenLikeSchema = mongoose.Schema({
+    userAddress: {
+        type: String,
+        required: true,
+        unique: false,
+        index: true,
+        lowercase: true,
+        trim: true
+    },
+    collectionId: {
+        type: Number,
+        required: true,
+        unique: false,
+        index: true,
+        lowercase: false,
+        trim: false
+    },
+    tokenId: {
+        type: Number,
+        required: true,
+        unique: false,
+        index: true,
+        lowercase: false,
+        trim: false
+    }
+}, { timestamps: true })
+
+//TokenLikesSchema.index({ user_address: 'text' })
+
+module.exports = mongoose.model('TokenLike', TokenLikeSchema)
