66fc60e482b1fe966a5f5a954f3dcd2d68fad025
Mico Häsänen <micohasanen@gmail.com>
2022-06-15 17:02:24 +0300

    Catch Moralis Error

--- utils/Moralis.js
+++ utils/Moralis.js
@@ -4,6 +4,8 @@ Moralis.start({
   serverUrl: process.env.MORALIS_SERVER, 
   appId: process.env.MORALIS_APP_ID, 
   masterKey: process.env.MORALIS_MASTER_KEY  
+}).catch((error) => {
+  console.error('Moralis Error:', error)
 })
 
 module.exports = { Moralis }
