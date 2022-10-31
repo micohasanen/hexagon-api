3f1b59409c4dbfe09381cc4bf4b82bb1ce1889f8
Recursive <recursivethedev@gmail.com>
2022-10-31 11:38:56 +0200

    cors

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const { initAgenda } = require("./providers/Agenda")
 
 const app = express()
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
